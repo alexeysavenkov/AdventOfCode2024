eda6fdab1d1968f479f12e4d13f11b0f7da313f0
Alexey Savenkov <alexey.savenkov7@gmail.com>
2024-12-05 01:31:22 +0100

    Create 4_2.py

---
+++ 4_2.py
@@ -0,0 +1,43 @@
+data = """MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX""".split("\n")
+
+possible_offsets = [
+    [(0,0), (1,1), (2,2), (2,0), (1,1), (0,2)]
+]
+
+possible_words = ["MASMAS", "SAMSAM", "SAMMAS", "MASSAM"]
+
+result = 0
+
+def is_valid_point(y,x):
+    if y < 0 or y >= len(data):
+        return False
+    if x < 0 or x >= len(data[y]):
+        return False
+    return True
+
+def get_word(y0,x0,offset):
+    res = ''
+    for x1, y1 in offset:
+        x = x0+x1
+        y = y0+y1
+        if is_valid_point(y,x):
+            res += data[y][x]
+    return res
+
+for y in range(len(data)):
+    for x in range(len(data[y])):
+        for offset in possible_offsets:
+            word = get_word(y,x,offset)
+            if word in possible_words:
+                result += 1
+
+print(result)
