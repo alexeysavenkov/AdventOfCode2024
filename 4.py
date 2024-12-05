91b2163d12cf1d81ec87d4e95414e06aa24bfbd9
Alexey Savenkov <alexey.savenkov7@gmail.com>
2024-12-05 01:26:33 +0100

    Update 4.py - fix letter count in offset config

--- 4.py
+++ 4.py
@@ -10,10 +10,10 @@ MAMMMXMMMM
 MXMXAXMASX""".split("\n")
 
 possible_offsets = [
-    [(0,0), (0,1), (0,2)],
-    [(0,0), (1,1), (2,2)],
-    [(0,0), (1,0), (2,0)],
-    [(0,0), (1,-1), (2,-2)]
+    [(0,0), (0,1), (0,2), (0,3)],
+    [(0,0), (1,1), (2,2), (3,3)],
+    [(0,0), (1,0), (2,0), (3,0)],
+    [(0,0), (1,-1), (2,-2), (3,-3)]
 ]
 
 possible_words = ["XMAS", "SAMX"]
@@ -40,6 +40,7 @@ for y in range(len(data)):
     for x in range(len(data[y])):
         for offset in possible_offsets:
             word = get_word(y,x,offset)
+            print("WORD", word)
             if word in possible_words:
                 result += 1
 
